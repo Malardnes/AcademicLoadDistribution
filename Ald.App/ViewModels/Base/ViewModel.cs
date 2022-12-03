ea676e0def544dac9c77aaab2ab28f0754f64afe
LioNord <mantradis.official@gmail.com>
2022-12-03 20:35:01 +0300

    Добавлен базовый класс модели представления.

---
+++ Ald.App/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Ald.App.ViewModels.Base
+{
+    /// <summary>
+    /// Базовый класс модели представления.
+    /// </summary>
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Событие изменения свойства объекта.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Метод - генерирующий событие изменения объекта.
+        /// </summary>
+        /// <param name="propertyName">Наименование свойства.</param>
+        protected virtual void InvokePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Функция проверки соответствия значения поля свойства к значению этого свойства.
+        /// </summary>
+        /// <typeparam name="T">Шаблонный тип.</typeparam>
+        /// <param name="field">Поле свойства.</param>
+        /// <param name="value">Значение свойства.</param>
+        /// <param name="propertyName">Наименование свойства.</param>
+        /// <returns>true - если удалось изменить значение свойства, false - если значение свойства соответствует полю.</returns>
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            InvokePropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
