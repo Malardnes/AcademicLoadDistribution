6071ba4c699469acdc4b9d1f61af18f743c8ea35
LioNord <mantradis.official@gmail.com>
2022-12-05 23:35:04 +0300

    Добавлены команды - закрытия окна и завершения работы приложения.

---
+++ Ald.App/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,14 @@
+﻿using Ald.App.Infrastructure.Commands.Base;
+
+namespace Ald.App.Infrastructure.Commands
+{
+    /// <summary>
+    /// Команда завершения работы программы.
+    /// </summary>
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => App.Current.Shutdown(0);
+    }
+}
---
+++ Ald.App/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,15 @@
+﻿using Ald.App.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace Ald.App.Infrastructure.Commands
+{
+    /// <summary>
+    /// Команда закрытия текущего окна.
+    /// </summary>
+    internal class CloseWindowCommand : Command
+    {
+        public override bool CanExecute(object parameter) => parameter is Window window && window is not null;
+
+        public override void Execute(object parameter) => App.CurrentWindow.Close();
+    }
+}
